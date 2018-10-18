f2883237dfdfdc4170d71e9bdf9f04e3c2fb59c7
yury.yudin <yury.yudin@adcubum.com>
2018-10-18 10:54:22 +0200

    Manager and persistence

---
+++ src/main/java/ems/domain/EventManager.java
@@ -0,0 +1,12 @@
+package ems.domain;
+
+import java.time.LocalDateTime;
+
+public interface EventManager {
+	
+	public Event build(LocalDateTime start, LocalDateTime end, String organizer, String title, String description);
+	public void create(Event event);
+	public void update(Event event);
+	public void delete(Event event);
+}
+
---
+++ src/main/java/ems/domain/EventPersistence.java
@@ -0,0 +1,8 @@
+package ems.domain;
+
+public class EventPersistence {
+	public void create(Event event) {};
+	public void update(Event event) {};
+	public void delete(Event event) {};
+	//public Event read() {};
+}
