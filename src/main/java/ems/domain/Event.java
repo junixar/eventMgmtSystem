b8480f6df787d50dcc2c7fcdae0f07df148e64f7
yury.yudin <yury.yudin@adcubum.com>
2018-10-18 10:36:27 +0200

    [EMS-1] added new entity

---
+++ src/main/java/ems/domain/Event.java
@@ -0,0 +1,59 @@
+package ems.domain;
+
+import java.time.LocalDateTime;
+
+public class Event {
+
+	private LocalDateTime start;
+	
+	private LocalDateTime end;
+	
+	private String organizer;
+	
+	private String title;
+	
+	private String description;
+
+	public LocalDateTime getStart() {
+		return start;
+	}
+
+	public void setStart(LocalDateTime start) {
+		this.start = start;
+	}
+
+	public LocalDateTime getEnd() {
+		return end;
+	}
+
+	public void setEnd(LocalDateTime end) {
+		this.end = end;
+	}
+
+	public String getOrganizer() {
+		return organizer;
+	}
+
+	public void setOrganizer(String organizer) {
+		this.organizer = organizer;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+
+
+}
