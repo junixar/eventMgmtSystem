b33dc9f1245b79e2e15a169568bf10edadb3da03
yury.yudin <yury.yudin@adcubum.com>
2018-10-18 11:06:33 +0200

    Testklasse erstellt

--- src/test/java/EventManagerImplTest.java
+++ src/test/java/EventManagerImplTest.java
@@ -1,6 +1,10 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+
+import java.time.LocalDateTime;
 
 import org.junit.Test;
+
+import ems.domain.Event;
 
 public class EventManagerImplTest {
 
@@ -10,8 +14,15 @@ public class EventManagerImplTest {
 	}
 
 	@Test
-	public void testCreate() {
+	public void test_CreateFailIfDateNotInFuture() {
+		Event event = new Event(//
+				LocalDateTime.now().plusDays(1), //
+				LocalDateTime.now().plusDays(2), //
+				"organizer", "Erstes Event", "Beispiel...");
+		
+
 		fail("Not yet implemented");
 	}
+	
 
 }
